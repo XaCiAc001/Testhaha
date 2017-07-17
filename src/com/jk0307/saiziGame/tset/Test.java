dc9ab4b1514093c28908f23c0f81ef60ce3eb764
Ho <Ho@Ho-PC>
2017-07-17 13:44:09 +0800

    提交测试

---
+++ src/com/jk0307/saiziGame/tset/Test.java
@@ -0,0 +1,11 @@
+package com.jk0307.saiziGame.tset;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("提交测试");
+	}
+
+}
